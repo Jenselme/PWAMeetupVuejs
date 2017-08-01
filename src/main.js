3b73b0139f376a3016ff94def167411ab2ed2085
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-01 20:43:52 +0200

    Remove router from main

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
-import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
@@ -12,7 +11,6 @@ Vue.use(VueMaterial)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
   components: { App }
 })
