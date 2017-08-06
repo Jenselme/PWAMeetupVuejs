d883a882d08731e9604de129c04dcdba48524b24
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-06 16:28:12 +0200

    Use my firebase instance

--- src/database.js
+++ src/database.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: 'AIzaSyCEq39n_xZW0QtRIX4_Gfbm0fpB21eTjq0',
-  authDomain: 'pwadojo.firebaseapp.com',
-  databaseURL: 'https://pwadojo.firebaseio.com',
-  projectId: 'pwadojo',
-  storageBucket: 'pwadojo.appspot.com',
-  messagingSenderId: '445295830164'
+  apiKey: 'AIzaSyAEallhRH7g4SvviMZOUZ606iXjRSrimoU',
+  authDomain: 'pwadojo-c881c.firebaseapp.com',
+  databaseURL: 'https://pwadojo-c881c.firebaseio.com',
+  projectId: 'pwadojo-c881c',
+  storageBucket: 'pwadojo-c881c.appspot.com',
+  messagingSenderId: '1056513791052'
 }
 
 const firebaseApp = firebase.initializeApp(config)
