c1bcd0e8ce9320bd1d536b7ba6e4ca3a8642bcb9
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-06 14:28:31 +0200

    Cache response from firebase

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <md-card v-for="(picture, key) in cats" v-bind:key="picture.id">
+    <md-card v-for="(picture, key) in getCats()" v-bind:key="picture.id">
       <md-card-media>
         <img v-bind:src="picture.url" alt="A cat">
       </md-card-media>
@@ -15,12 +15,31 @@ export default {
   name: 'hello',
   data () {
     return {
+      getCats () {
+        if (navigator.onLine) {
+          return this.cats
+        }
+
+        return JSON.parse(localStorage.getItem('cats'))
+      }
+    }
+  },
+  methods: {
+    saveCatsToCache () {
+      if (navigator.onLine) {
+        this.$firebaseRefs.cats.once('value', snapshot => {
+          localStorage.setItem('cats', JSON.stringify(snapshot.val()))
+        })
+      }
     }
   },
   firebase: {
     cats: {
       source: db.ref('pictures')
     }
+  },
+  mounted () {
+    this.saveCatsToCache()
   }
 }
 </script>
