93059096249d7b610bcf8113bb63b62af4d1c45b
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-06 15:46:57 +0200

    Add the fields and button to add a cat

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>Add a cat</h1>
+
+    <md-input-container>
+        <label>Title</label>
+        <md-input placeholder="Title"></md-input>
+    </md-input-container>
+
+    <md-input-container>
+        <label>Image URL</label>
+        <md-input placeholder="Image URL"></md-input>
+    </md-input-container>
+
+    <md-button class="md-raised md-primary add-button">
+      Save
+    </md-button>
   </div>
 </template>
 
