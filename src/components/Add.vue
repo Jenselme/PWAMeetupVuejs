e6e8c37b2a2158cf569e4f9cef48810fe3552fca
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-06 16:28:19 +0200

    Can add new cats

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -4,25 +4,46 @@
 
     <md-input-container>
         <label>Title</label>
-        <md-input placeholder="Title"></md-input>
+        <md-input placeholder="Title" v-model="title"></md-input>
     </md-input-container>
 
     <md-input-container>
         <label>Image URL</label>
-        <md-input placeholder="Image URL"></md-input>
+        <md-input placeholder="Image URL" v-model="url"></md-input>
     </md-input-container>
 
-    <md-button class="md-raised md-primary add-button">
+    <md-button class="md-raised md-primary"
+               :disabled="!title || !url"
+               v-on:click="pushCat()">
       Save
     </md-button>
   </div>
 </template>
 
 <script>
+import { db } from '../database'
+
 export default {
   name: 'hello',
   data () {
-    return {}
+    return {
+      title: undefined,
+      url: undefined
+    }
+  },
+  methods: {
+    pushCat () {
+      this.$firebaseRefs.cats.push({
+        url: this.url,
+        title: this.title
+      })
+      this.$router.push('/')
+    }
+  },
+  firebase: {
+    cats: {
+      source: db.ref('pictures')
+    }
   }
 }
 </script>
